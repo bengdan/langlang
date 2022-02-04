4e9055b5144cda15af6a41d455e799700f877332
manji <a1575208138@gmail.com>
2022-02-05 01:00:42 +0800

    only change kernel

--- build/Lede_nanopi_r4s/diy-part.sh
+++ build/Lede_nanopi_r4s/diy-part.sh
@@ -3,12 +3,12 @@
 # DIY扩展二合一了，在此处可以增加插件
 #
 echo "close_dhcp" > package/base-files/files/etc/closedhcp                                         # 关闭DHCP服务
-sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 sed -i "/uci commit fstab/a\uci commit network" $ZZZ
-sed -i "/uci commit network/i\uci set network.lan.ipaddr='192.168.66.1'" $ZZZ                      # IPv4 地址(openwrt后台地址)
+sed -i "/uci commit network/i\uci set network.lan.ipaddr='192.168.2.1'" $ZZZ                      # IPv4 地址(openwrt后台地址)
 sed -i "/uci commit network/i\uci set network.lan.netmask='255.255.255.0'" $ZZZ                   # IPv4 子网掩码
-sed -i "/uci commit network/i\uci set network.lan.gateway='192.168.66.1'" $ZZZ                     # IPv4 网关
-sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.66.255'" $ZZZ                 # IPv4 广播
+sed -i "/uci commit network/i\uci set network.lan.gateway='192.168.2.1'" $ZZZ                     # IPv4 网关
+sed -i "/uci commit network/i\uci set network.lan.broadcast='192.168.2.255'" $ZZZ                 # IPv4 广播
 sed -i "/uci commit network/i\uci set network.lan.dns='114.114.114.114 223.5.5.5'" $ZZZ           # DNS(多个DNS要用空格分开)
 sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ                              # 去掉LAN口使用内置的 IPv6 管理
 
@@ -16,9 +16,9 @@ sed -i "/uci commit network/i\uci set network.lan.delegate='0'" $ZZZ            
 sed -i '/ip6assign/d' package/base-files/files/bin/config_generate
 
 sed -i 's/luci-theme-bootstrap/luci-theme-opentomcat/g' feeds/luci/collections/luci/Makefile           
-sed -i "s/OpenWrt /蛮大人 $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ
+sed -i "s/OpenWrt /大灰狼 $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ         
 
-sed -i "/uci commit system/i\uci set system.@system[0].hostname='Manji-Cat'" $ZZZ
+sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-DHL'" $ZZZ               
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                            # 设置密码为空
 
